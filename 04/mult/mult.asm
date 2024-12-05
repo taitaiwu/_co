39dc351f93708d89e5f3fca54b36bdb1aebebdb5
taitaiwu <s919075@stu.chsc.tw>
2024-12-05 11:23:10 +0800

    homework9 update

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,20 +7,32 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+//int R2 = 0; 
 @2 
 M=0
-@loop
-@0 
-D=M 
-@exit1 
-D;JLE
-@1 
-D=M 
-@2 
-M=M+D
-@0 
-M=M-1
-@loop
-0;JUP
-@exit1
-0;JMP
+
+(loop)
+    //if (R0 <= 0) goto exit1;
+    @0 
+    D=M 
+    @exit1 
+    D;JLE
+
+    //R2 = R2 + R1
+    @1 
+    D=M 
+    @2 
+    M=M+D
+
+    //R0 = R0 - 1
+    @0 
+    M=M-1
+
+    //goto loop;
+    @loop
+
+0;JUP 
+
+(exit1)
+    0;JMP
